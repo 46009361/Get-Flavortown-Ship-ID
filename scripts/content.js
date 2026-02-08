f3cfef51ec5cb589ab9d1237192b0c08eb3f9e02
46009361 <165697051+46009361@users.noreply.github.com>
2026-02-07 21:54:08 -0800

    Add safeguard against missing element

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,22 @@
 // content.js
 
 async function ship(doc) {
-  if (!doc) {
+  const b64 = doc?.querySelector("#suggestion_token").value.split("--")[0];
+  
+  if (!b64) {
     return;
   }
 
-  const b64 = doc.querySelector("#suggestion_token").value.split("--")[0];
   const card = doc.querySelectorAll(".votes-new__project-card-stat")[1];
   const clone = card.cloneNode(true);
-  const shipImg = await (await fetch(chrome.runtime.getURL("images/ship.svg"))).text();
+  const response = await fetch(chrome.runtime.getURL("images/ship.svg"));
+  const shipImg = await response.text();
   const parser = new DOMParser();
   const svgDoc = parser.parseFromString(shipImg, "image/svg+xml");
   const svg = svgDoc.documentElement;
   clone.querySelector("svg").replaceWith(svg);
   const span = clone.querySelector("span");
-  const shipEventId = JSON.parse(atob(b64))?.ship_event_id;
+  const shipEventId = JSON.parse(atob(b64)).ship_event_id;
   console.log(shipEventId);
   span.textContent = chrome.i18n.getMessage("ship", [shipEventId]);
   card.parentElement.appendChild(clone);
